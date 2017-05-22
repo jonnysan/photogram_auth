c3eacf084a6230616701a3dcc82bb32961c64f36
Jonny San <jonny.san@kellogg.northwestern.edu>
2017-05-22 15:45:26 -0500

    new commit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+  # user_id: presence
+  validates :user_id, :presence => true
+# photo_id: presence
+validates :photo_id, :presence => true
+# body: presence
+validates :body, :presence => true
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,10 @@
 class Photo < ApplicationRecord
+  # Photos have many comments, a comment belongs to a photo
+  has_many :comments
+  # Photos have many likes, a like belongs to a photo
+  has_many :likes
+  # Similarly, Photos have many fans through likes (source: user):
+  has_many :fans, :through => :likes, :source => :user
+  # user_id: presence
+  validates :user_id, :presence => true
 end
